ec8a2c7dfe60b2962c63d1b09aefdce7925d0ffd
必应 <38268998+bb1026@users.noreply.github.com>
2025-06-06 07:01:41 +0800

    Delete js/theme.js

--- js/theme.js
+++
@@ -1,7 +0,0 @@
-/* 暗黑主题 */
-document.addEventListener('DOMContentLoaded', () => {
-  const isDark = localStorage.getItem('darkMode') === 'true';
-  if (isDark) {
-    document.body.classList.add('dark');
-  }
-});
