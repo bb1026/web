989b1a38c08d68fc9d9bcb3182408b6b483a0197
必应 <38268998+bb1026@users.noreply.github.com>
2025-07-12 17:03:37 +0800

    Delete Cloudflare/pan/auth.js

--- Cloudflare/pan/auth.js
+++
@@ -1,7 +0,0 @@
-import { detectRoleFromPassword, jsonResponse } from './utils.js';
-
-export async function handleAuth(request, accessMap) {
-  const pwd = (await request.text()).trim();
-  const { role, remark } = detectRoleFromPassword(accessMap, pwd);
-  return jsonResponse({ role, remark });
-}
