ea17c528a487225251dc13ad7c73e204effd0960
必应 <38268998+bb1026@users.noreply.github.com>
2026-04-09 16:46:05 +0800

    Update lunar.js

--- generate/lib/lunar.js
+++ generate/lib/lunar.js
@@ -1,16 +1,9 @@
-const lunarUrl = "https://0515364.xyz/calendar/js/lunarCalendar.js"
+// lib/lunar.js
+import { getCalendar } from "../calendar/js/lunarCalendar.js"
 
 export async function getLunar(year) {
-  const res = await fetch(lunarUrl)
-  const code = await res.text()
-
-  const module = {}
-  const exports = {}
-  eval(code) // lunarCalendar.js 必须导出 getCalendar(year)
-
-  if (typeof module.getCalendar === "function") {
-    return module.getCalendar(year)
+  if (typeof getCalendar === "function") {
+    return getCalendar(year)
   }
-
   return []
 }
