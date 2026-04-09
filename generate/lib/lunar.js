898985c3093b6ae3ee1263bbcaac0d4a5688eea5
必应 <38268998+bb1026@users.noreply.github.com>
2026-04-09 17:00:18 +0800

    Delete generate directory

--- generate/lib/lunar.js
+++
@@ -1,9 +0,0 @@
-// lib/lunar.js
-import { getCalendar } from "../calendar/js/lunarCalendar.js"
-
-export async function getLunar(year) {
-  if (typeof getCalendar === "function") {
-    return getCalendar(year)
-  }
-  return []
-}
